022d24ce6790133af9c878a6af5ded943c36e7a1
Alex Silva <alexsaraujo92@gmail.com>
2021-09-22 19:43:09 -0300

    altera dbConnec

--- dbConnection.js
+++ dbConnection.js
@@ -2,7 +2,7 @@
 const{ Pool } = require('pg')
 
 const cliente = new Pool({
-    connectionString: 'postgres://muprdwnmylnzua:50f952bfa6f300e364bc6d471d890c5e6527b581ab8224d603c39f0911fa29f4@ec2-34-233-105-94.compute-1.amazonaws.com:5432/d9cdpvdi13op2m',
+    connectionString: 'postgres://dpegctbpjjjlkn:9695b32527056390e7808db1a1490260bf89eeb8dc2bb1a66e89ea75a31a55e2@ec2-34-233-105-94.compute-1.amazonaws.com:5432/diqqu48hjq0ev',
     ssl:{
         rejectUnauthorized: false
     }
